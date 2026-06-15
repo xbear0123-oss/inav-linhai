5ffe93bce49d95b8ce1ca805e680539509497f1a
xbear0123-oss <xbear0123@gmail.com>
2026-06-15 18:05:24 +0800

    2026.06.15新增加了BearF405Wing控target，去掉了osd，加入了2个pinio.

--- src/main/target/MATEKF405TE/target.h
+++ src/main/target/MATEKF405TE/target.h
@@ -41,12 +41,10 @@
 #define SPI1_MISO_PIN   	    PB4
 #define SPI1_MOSI_PIN   	    PA7
 
-/*
 #define USE_IMU_ICM42605
 #define IMU_ICM42605_ALIGN      CW270_DEG_FLIP
 #define ICM42605_SPI_BUS        BUS_SPI1
 #define ICM42605_CS_PIN         PC14
-*/
 
 #define USE_IMU_BMI270
 #define IMU_BMI270_ALIGN        CW270_DEG_FLIP
@@ -54,11 +52,11 @@
 #define BMI270_CS_PIN           PC14
 
 
-/*
+
 #define USE_MAX7456
 #define MAX7456_SPI_BUS         BUS_SPI1
 #define MAX7456_CS_PIN          PB12
-*/
+
 // *************** SPI2 Flash/SD Card  ****************
 #define USE_SPI_DEVICE_2
 #define SPI2_SCK_PIN            PB13
@@ -138,7 +136,7 @@
 #define SERIALRX_PROVIDER       SERIALRX_CRSF
 #define SERIALRX_UART           SERIAL_PORT_USART2
 
-// *************** ADC ***************************.
+// *************** ADC ***************************
 #define USE_ADC
 #define ADC_INSTANCE                ADC1
 #define ADC1_DMA_STREAM             DMA2_Stream4
@@ -154,10 +152,8 @@
 // *************** PINIO ***************************
 #define USE_PINIO
 #define USE_PINIOBOX
-//#define PINIO1_PIN                  PA4
-//#define PINIO2_PIN                  PB5
-#define PINIO1_PIN                  PB0
-#define PINIO2_PIN                  PB12
+#define PINIO1_PIN                  PA4
+#define PINIO2_PIN                  PB5
 
 // *************** LEDSTRIP ************************
 #define USE_LED_STRIP
